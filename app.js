5a8bb4d1aed7c2af1379c04e8435cd4c8b94a5fb
Evelina <evelinabrinkemo@gmail.com>
2018-05-23 21:16:15 +0200

    playing sounds repeatedly

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
-let pad1 = document.querySelector('#pad1');
-let pad2 = document.querySelector('#pad2');
-let pad3 = document.querySelector('#pad3');
-let pad4 = document.querySelector('#pad4');
-
 let padDiv = document.querySelector('#padDiv');
 
 let audio0 = new Audio('sounds/clap.wav');
@@ -10,11 +5,16 @@ let audio1 = new Audio('sounds/boom.wav');
 let audio2 = new Audio('sounds/hihat.wav');
 let audio3 = new Audio('sounds/kick.wav');
 
-let sounds = [audio0, audio1, audio2, audio3]
+let sounds = [audio0, audio1, audio2, audio3];
+
+let currentlyPlaying;
 
 for (let i = 0; i < padDiv.children.length; i++) {
-    padDiv.children[i].addEventListener('click', function (){
-        console.dir(this);
-        sounds[i].play();
-    })
+  padDiv.children[i].addEventListener('click', function(){
+    if (currentlyPlaying != undefined) {
+      currentlyPlaying.load();
+    }
+    currentlyPlaying = sounds[i];
+    sounds[i].play();
+  });
 }
